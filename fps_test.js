d98ab1556c7e258f60718222acfaf17256d9ab6e
NightlyRevenger <NightlyRevenger@yandex.ru>
2016-12-28 16:40:25 +0300

    Update fps_test.js

--- fps_test.js
+++ fps_test.js
@@ -6,6 +6,124 @@ var FPS1intervalID;
 var FPS2intervalID;
 
 var gui, guiElements, param = { color: '0xffffff' };
+var render_stats1;
+var render_stats2;
+
+var camera, scene, renderer1, renderer2;
+var mfbCube;
+
+function init()
+{
+	var Width=document.getElementById("container1").offsetWidth;
+	var Height=document.getElementById("container1").offsetHeight 
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, Width/Height, 0.1, 1000 );
+	
+	mfbCube;
+	var loader = new THREE.TextureLoader();
+	loader.load( 'mfb_logo_256.jpg', function ( texture ) 
+	{
+
+		var geometry = new THREE.BoxGeometry( 80, 31.2, 15.6 );
+		var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+		mfbCube = new THREE.Mesh( geometry, material );
+		scene.add(mfbCube);
+	} );			
+	
+	camera.position.z = 100;
+	camera.position.y = 18;
+	camera.rotation.x = -15 * (Math.PI / 180);
+
+	elt1=document.getElementById("container1");
+	render_stats1 = new Stats();
+	render_stats1.showPanel( 0 );
+	render_stats1.domElement.style.position = 'absolute';
+	render_stats1.domElement.style.top = String(elt1.offsetTop+1)+'px';
+	render_stats1.domElement.style.left = String(elt1.offsetLeft)+'px';
+	render_stats1.domElement.style.zIndex = 100;
+	elt1.appendChild( render_stats1.dom );
+	
+	elt2=document.getElementById("container2");
+	render_stats2 = new Stats();
+	render_stats2.showPanel( 0 );
+	render_stats2.domElement.style.position = 'absolute';
+	render_stats2.domElement.style.top = String(elt2.offsetTop+1)+'px';
+	render_stats2.domElement.style.left = String(elt2.offsetLeft)+'px';
+	render_stats2.domElement.style.zIndex = 100;
+	elt2.appendChild( render_stats2.dom );
+	
+	renderer1 = new THREE.WebGLRenderer( { antialias: true } );
+	renderer1.setClearColor( 0x000000 );
+	renderer1.setPixelRatio( window.devicePixelRatio );
+	renderer1.setSize( Width,Height );
+	document.getElementById("container1").appendChild( renderer1.domElement );
+
+	renderer2 = new THREE.WebGLRenderer( { antialias: false } );
+	renderer2.setClearColor( 0x000000 );
+	renderer2.setPixelRatio( window.devicePixelRatio );
+	renderer2.setSize( Width,Height );
+	document.getElementById("container2").appendChild( renderer2.domElement );
+}
+
+function animate() 
+{
+	var lastTime = performance.now();
+	var deltaTime=performance.now();
+	
+	var render1 = function () 
+	{
+		
+		render_stats1.begin();			
+		if (typeof mfbCube !== 'undefined') 
+		{
+			mfbCube.position.x=cubePosX;
+		}
+		
+		render_stats1.end();			
+		
+		renderer1.render(scene, camera);
+		
+		deltaTime=performance.now()-lastTime;
+		
+		if(1000/(FPS2+10)-deltaTime<0)
+		{
+			render_stats2.begin()
+			lastTime = performance.now();
+			render_stats2.end();
+			renderer2.render(scene, camera);		
+		}
+
+	};
+	
+	var render2 = function () 
+	{
+		
+		render_stats2.begin();			
+		if (typeof mfbCube !== 'undefined') 
+		{
+			mfbCube.position.x=cubePosX;
+		}
+		
+		render_stats2.end();			
+		
+		renderer2.render(scene, camera);
+
+	};
+	
+	tmpIntervalID=setInterval( function () 
+	{
+		requestAnimationFrame( render1 );
+		
+	}, 1000/FPS1 );
+	
+	/*tmpIntervalID=setInterval( function () 
+	{
+		requestAnimationFrame( render2 );
+		
+	}, 1000/FPS2 );//*/
+	
+}
+
 function StartGL(fpsLimit, tgCanvas)  
 {
 			fpsLimit=1000/fpsLimit;
@@ -15,6 +133,9 @@ function StartGL(fpsLimit, tgCanvas)
 			var scene = new THREE.Scene();
 			camera = new THREE.PerspectiveCamera( 75, Width/Height, 0.1, 1000 );
 			//camera = new THREE.OrthographicCamera( Width / -8, Width / 8, Height / 8, Height / -8, 1, 100 );
+			
+			if (typeof renderer !== 'undefined') 
+					renderer.dispose();
 
 			var renderer = new THREE.WebGLRenderer({ antialias: true });
 			
@@ -38,7 +159,7 @@ function StartGL(fpsLimit, tgCanvas)
 
 			var mfbCube;
 			var loader = new THREE.TextureLoader();
-			loader.load( 'mfb_logo_256_2.jpg', function ( texture ) 
+			loader.load( 'mfb_logo_256.jpg', function ( texture ) 
 			{
 
 				var geometry = new THREE.BoxGeometry( 80, 31.2, 15.6 );
@@ -60,37 +181,34 @@ function StartGL(fpsLimit, tgCanvas)
 			render_stats.domElement.style.zIndex = 100;
 			elt.appendChild( render_stats.dom );
 			
-			lastTime = Date.now()
-			requestAnimationFrame = function ( callback ) {
-			  var currTime = Date.now(), timeToCall = Math.max( 0, 1000/120.0 - ( currTime - lastTime ) );
-			  var id = self.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
-			  lastTime = currTime + timeToCall;
-			  return id;
-			};
 			
-
+		var deltaTime=performance.now()
+		var lastTime = performance.now();
 		var render = function () 
 		{
 			
-			render_stats.begin();
-			
+			render_stats.begin();			
 			if (typeof mfbCube !== 'undefined') 
 			{
 				mfbCube.position.x=cubePosX;
 			}
 			
-			render_stats.end();
-
+			//deltaTime=performance.now()-lastTime;
+			//lastTime = performance.now();
+			//sleep(fpsLimit-deltaTime);
+			
+			render_stats.end();			
+			
 			renderer.render(scene, camera);
 
 		};
 		
-		//render();
 		requestAnimationFrame( render );
 		
 		tmpIntervalID=setInterval( function () 
 		{
 			requestAnimationFrame( render );
+			
 		}, fpsLimit );
 		
 		if(tgCanvas=="container2")
@@ -99,8 +217,11 @@ function StartGL(fpsLimit, tgCanvas)
 
 function sleep(ms) 
 {
-	ms += new Date().getTime();
-	while (new Date() < ms){}
+	var ms2 = ms + performance.now();
+	while (performance.now() < ms2)
+	{
+		//console.log("sleeping ")
+	}
 }
 
 function moveCube(speed) 
@@ -109,7 +230,7 @@ function moveCube(speed)
 	if(cubePosX> 420.0)
 	{
 		cubePosX=-420.0;
-	}	
+	}
 }
 
 function fpsLock(fps) 
@@ -123,3 +244,4 @@ function fpsLock(fps)
 		return id;
 	};	
 }
+	
